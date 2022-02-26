5b1311839f4e65086a034537de98f9d10e9ea7e3
sercel23 <ercelserap@gmail.com>
2022-02-26 22:07:34 +0300

    Add menu

--- app/src/main/java/com/example/mytravelbook/MainActivity.kt
+++ app/src/main/java/com/example/mytravelbook/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.mytravelbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.place_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_place) {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
