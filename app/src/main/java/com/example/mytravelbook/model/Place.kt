38a0f21c9b0453e3ed2da0dbcdf3667104c479ed
sercel23 <ercelserap@gmail.com>
2022-03-02 00:41:44 +0300

    Add Room entity

---
+++ app/src/main/java/com/example/mytravelbook/model/Place.kt
@@ -0,0 +1,18 @@
+package com.example.mytravelbook.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class Place (
+    @ColumnInfo(name="name")
+    var name: String,
+    @ColumnInfo(name = "latitude")
+    var latitude: Double,
+    @ColumnInfo(name ="longitude")
+    var longitude: Double
+    ) {
+    @PrimaryKey(autoGenerate = true)
+    var id=0
+}
